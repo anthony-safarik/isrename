67771d2dce67d6c4c580592bc0fd9e96ecb49440
Anthony Safarik <anthony.safarik@gmail.com>
2019-08-16 08:32:23 -0700

    Added arg parser

--- isrename.py
+++ isrename.py
@@ -3,7 +3,14 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='This script renames image sequences')
-parser.add_argument('-f','--folder', help='folder path',required=True)
+parser.add_argument('-i','--inpath', help='enter folder path containing an image sequence',required=True)
+parser.add_argument('-e','--ext', help='enter the file extension to operate on',required=True)
+parser.add_argument('-s','--start', help='start frame number',required=False)
 
 args = parser.parse_args()
-print (args.folder)
+
+inpath = args.inpath
+ext = args.ext
+start = args.start
+
+print (inpath,ext,start)
